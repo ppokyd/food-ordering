bc9fc32c6b46f505c35e44030d4c600a75df1510
Petro Pokydko <petro.pokydko@gmail.com>
2017-12-15 11:49:32 +0200

    hardcode

--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -5,11 +5,6 @@ import * as firebase from 'firebase/app';
 @Injectable()
 export class AppService {
   private user = {email: ''};
-  private admins = [
-    'petropo@playbuzz.com',
-    'petro.pokydko@gmail.com',
-    'opiskozub@gmail.com'
-  ];
 
   constructor(
     public afAuth: AngularFireAuth
@@ -22,12 +17,12 @@ export class AppService {
   }
 
   isAdmin() {
-    return this.admins.find(a => a === this.user.email) ? true : false;
+    return this.user.email.startsWith('petropo');
   }
 
   isPbUser() {
     return this.user.email.endsWith('@playbuzz.com') ||
-      this.user.email.endsWith('opiskozub@gmail.com');
+      this.user.email.startsWith('opiskozub');
   }
 
   logout() {
